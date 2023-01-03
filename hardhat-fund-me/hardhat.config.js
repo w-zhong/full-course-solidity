bf6bb987c430044bb7503720044fab2124fff0e0
0x621e <0x621e@gmail.com>
2023-01-03 13:31:57 +0800

    update

--- hardhat-fund-me/hardhat.config.js
+++ hardhat-fund-me/hardhat.config.js
@@ -1,6 +1,23 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("hardhat-deploy");
+require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  defaultNetwork: "goerli",
+  networks: {
+    goerli: {
+      url: process.env.RPC_URL_GOERLI,
+      accounts: [process.env.PRIVATE_KEY],
+      chainId: 5,
+    },
+  },
   solidity: "0.8.17",
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+  },
 };
